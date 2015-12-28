81c883051d899fabd6966843c9d885861ac0f0c5
肖楠 <2535865932@qq.com>
2015-12-28 08:38:56 +0800

    bai

---
+++ baibai/baibai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baibai
+//
+//  Created by scjy on 15/12/28.
+//  Copyright (c) 2015年 芒果iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
